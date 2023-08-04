26c3bae2a4ff93278450de5876023f9393dc5a70
Sam Tran <samuelttran@gmail.com>
2023-08-03 18:36:01 -0600

    Write method: check_stock, for final_practice.rb

--- final_practice.rb
+++ final_practice.rb
@@ -79,15 +79,26 @@ puts squared_11
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
-
-check_stock(4, "Coffee");
+def check_stock(amount, item)
+    status = ""
+    if amount == 0
+        status = "- OUT of stock"
+    elsif amount >= 4
+        status = "is stocked"
+    else
+        status = "- running LOW"
+    end
+    return "#{item} #{status}"
+end
+    
+puts check_stock(4, "Coffee");
 # => "Coffee is stocked"
 
-check_stock(3, "Tortillas");
+puts check_stock(3, "Tortillas");
 # => "Tortillas - running LOW"
 
-check_stock(0, "Cheese");
+puts check_stock(0, "Cheese");
 # => "Cheese - OUT of stock!"
 
-check_stock(1, "Salsa");
+puts check_stock(1, "Salsa");
 # => "Salsa - running LOW"
